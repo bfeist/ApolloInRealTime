60d48c09f0f73f2574e5ba198939a8e4a95d06a7
Ben Feist <bf@benfeist.com>
2018-12-23 21:10:11 -0500

    Merged commentary. Now cleaning commentary datafile manually. Merged photos into A11_photos.xlsx. This now exports with BuildAllHTML.py to photo datafile. Modified javascript to use ALSJ as photo source

--- scripts/move_used_images.py
+++ scripts/move_used_images.py
@@ -5,7 +5,7 @@ import csv
 
 sourcePath = r'E:\temp\A11_images\A11_images\www.hq.nasa.gov\office\pao\History\alsj\a11\\'
 destPath = r'D:\Apollo_11\_website\_webroot\mission_images\\'
-inputFilePath = "../MISSION_DATA/scraped_photoinfo.csv"
+inputFilePath = "../MISSION_DATA/scraped_data/scraped_photoinfo.csv"
 reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
 for row in reader:
     fileToMove = row[2]
